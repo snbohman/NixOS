9059c6ea5a363c9471aa4c726bc55e64edf42b50
snbohman <sixten.bohman.08@gmail.com>
2025-05-04 12:23:56 +0200

    2025-05-04

--- rebuild.sh
+++ rebuild.sh
@@ -3,7 +3,7 @@
 cd ~/dotfiles
 sudo nixos-generate-config --show-hardware-config > hardware-configuration.nix
 git add --all
-git commit -m "$1"
+git commit -m "$(date +%F)"
 git push
 
 sudo nixos-rebuild switch --flake .
