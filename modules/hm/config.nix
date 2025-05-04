fcfdd56c281eea0eb475a994a3d5fccbe856e5a4
snbohman <sixten.bohman.08@gmail.com>
2025-05-04 18:14:14 +0200

    2025-05-04 18:14:14

--- modules/hm/config.nix
+++ modules/hm/config.nix
@@ -4,28 +4,17 @@
 {
     home.file = {
     	".config/nvim" = {
-	    source = ./nvim;
-	    recursive = true;
-	};
+	        source = ./nvim;
+	        recursive = true;
+	    };
 
-    	".config/nvim/lua" = {
-	    source = ./nvim/lua;
-	    recursive = true;
-	};
+        ".config/hypr/userprefs.conf" = lib.mkForce {
+	        source = ./hypr/userprefs.conf;
+	    };
 
-    	# ".config/nvim/lua/plugins" = {
-	#     source = ./nvim/lua/plugins;
-	#     recursive = true;
-	# };
-
-    	# ".config/nvim/lua/config" = {
-	#     source = ./nvim/lua/config;
-	#     recursive = true;
-	# };
-
-    	".config/hypr/userprefs.conf" = lib.mkForce {
-	    source = ./hypr/userprefs.conf;
-	};
+        ".config/kitty/kitty.conf" {
+	        source = ./kitty/kitty.conf;
+            recursive = true;
+	    };
     };
-
 }
