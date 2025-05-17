ed8fa7dfe9fbc098874ebc38f7c9ceeb2b0f7934
snbohman <sixten.bohman.08@gmail.com>
2025-05-17 11:01:24 +0200

    2025-05-17 11:01:24

--- modules/hm/config/nvim/lua/chadrc.lua
+++ modules/hm/config/nvim/lua/chadrc.lua
@@ -14,7 +14,12 @@ M.base46 = {
 	},
 }
 
-M.ui.tabufline.enabled = false
-M.term.sizes.vsp = 0.5
+M.nvdash = { load_on_startup = false }
+
+M.ui = {
+    tabufline = {
+        enabled = false
+    }
+}
 
 return M
