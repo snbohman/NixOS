235de31cfb8e5971a3a378416c4f7faf7974d72a
snbohman <sixten.bohman.08@gmail.com>
2025-05-20 13:50:10 +0200

    2025-05-20 13:50:10

--- modules/system/keyboard.nix
+++ modules/system/keyboard.nix
@@ -3,6 +3,6 @@
 {
     services.xserver.xkb.extraLayouts.seCustom = {
         description = "Swedish (Custom)";
-        symbolsFile = ./xkb/swedishCustom
+        symbolsFile = ./xkb/swedishCustom;
     };
 }
