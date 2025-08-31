f2a897299238a2eb9486411254b5b5c048950505
snbohman <sixten.bohman.08@gmail.com>
2025-08-31 18:47:19 +0200

    2025-08-31 18:47:19

--- modules/hm/links.nix
+++ modules/hm/links.nix
@@ -1,27 +1,14 @@
 { lib, pkgs, config, ... }:
 
 {
-    aa
     home.file = {
     	".config/nvim" = {
 	        source = ./config/nvim;
 	        recursive = true;
 	    };
 
-        ".config/hypr/userprefs.conf" = lib.mkForce {
-	        source = ./config/hypr/userprefs.conf;
-	    };
-
-        ".config/hypr/keybindings.conf" = lib.mkForce {
-	        source = ./config/hypr/keybindings.conf;
-	    };
-
         ".zshrc" = lib.mkForce {
 	        source = ./config/zsh/zshrc;
-	    };
-
-        ".config/kitty/kitty.conf" = {
-	        source = ./config/kitty/kitty.conf;
 	    };
 
         ".mozilla/firefox/q8pmdenq.default/chrome" = {
@@ -32,11 +19,5 @@
         ".gitconfig" = {
             source = ./config/git/gitconfig;
         };
-    };
-
-    programs.tmux = {
-        enable = true;
-        plugins = [ ];
-        extraConfig = builtins.readFile ./config/tmux/tmux.conf;
     };
 }
