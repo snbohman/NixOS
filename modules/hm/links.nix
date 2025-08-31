46f8377650872055d33ffe1bbb0cfe639a474ed5
snbohman <sixten.bohman.08@gmail.com>
2025-08-31 18:47:59 +0200

    2025-08-31 18:47:59

--- modules/hm/links.nix
+++ modules/hm/links.nix
@@ -7,10 +7,6 @@
 	        recursive = true;
 	    };
 
-        ".zshrc" = lib.mkForce {
-	        source = ./config/zsh/zshrc;
-	    };
-
         ".mozilla/firefox/q8pmdenq.default/chrome" = {
             source = ./config/firefox;
             recursive = true;
