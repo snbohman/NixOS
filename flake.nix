beb923070eba696a1307983187d99cd71fef909e
snbohman <sixten.bohman.08@gmail.com>
2025-08-31 18:54:58 +0200

    2025-08-31 18:54:58

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
         nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
 
         home-manager = {
-            url = "github:nix-community/home-manager-25.05";
+            url = "github:nix-community/home-manager";
             inputs.nixpkgs.follows = "nixpkgs";
         };
     };
