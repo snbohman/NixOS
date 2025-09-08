34d3e6cd79018803a6a11d7e26e191d6cb0b7171
snbohman <sixten.bohman.08@gmail.com>
2025-09-08 17:18:33 +0200

    2025-09-08 17:18:33

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -30,7 +30,7 @@
     ./waybar                          # status bar
     ./waypaper.nix                    # GUI wallpaper picker
     ./xdg-mimes.nix                   # xdg config
-    ./xremap.nix                      # remap keys
+    ./xremap/xremap.nix               # remap keys
     ./zsh                             # shell
   ];
 }
