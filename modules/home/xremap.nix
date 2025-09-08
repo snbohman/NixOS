018ed18b21876f85f4721dfe069bcfa884dc929e
snbohman <sixten.bohman.08@gmail.com>
2025-09-08 17:02:18 +0200

    2025-09-08 17:02:18

--- modules/home/xremap.nix
+++ modules/home/xremap.nix
@@ -10,14 +10,25 @@
         withHypr = true;
 
         config = {
-            keymap = [
-                {
-                    name = "testing";
-                    remap = {
-                        CapsLock = "Esc";
+            modmap = [ {
+                name = "Bracket Remap";
+                remap = {
+                    KEY_LEFTBRACE = "LeftBrace";
+                    KEY_RIGHTBRACE = "RightBrace";
+                };
+            } ];
+
+            keymap = [ {
+                name = "Custom Symbols Key";
+                remap = {
+                    KEY_BACKSLASH = {
+                        press = ["less"];                   # normal press -> <
+                        "Shift-press" = ["greater"];        # Shift + press -> >
+                        "RAlt-press" = ["asciitilde"];      # Right Alt + press -> ~
+                        "Shift-RAlt-press" = ["bar"];       # Shift + Right Alt + press -> |
                     };
-                }
-            ];
+                };
+            } ];
         };
     };
 }
