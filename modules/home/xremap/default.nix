b3f41056920cec8557ed92e8015eeb86617bbee5
snbohman <sixten.bohman.08@gmail.com>
2025-09-08 17:38:52 +0200

    2025-09-08 17:38:52

--- modules/home/xremap/default.nix
+++ modules/home/xremap/default.nix
@@ -8,6 +8,7 @@
     services.xremap = {
         enable = true;
         withHypr = true;
+        deviceNames = [ "usb-BY_Tech_Gaming_Keyboard-event-kbd" ];
         config = import ./mapping.nix;
     };
 }
