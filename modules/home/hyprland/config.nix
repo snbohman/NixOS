4f4c2c14b06f153d054426cfdcea59ea8e318e37
snbohman <sixten.bohman.08@gmail.com>
2025-09-08 19:00:47 +0200

    2025-09-08 19:00:47

--- modules/home/hyprland/config.nix
+++ modules/home/hyprland/config.nix
@@ -144,7 +144,7 @@ in
       };
 
       bind = [
-        "$mainMod SHIFT, K, hyprctl switchxkblayout by-tech-gaming-keyboard next"
+        "$mainMod SHIFT, K, exec, hyprctl switchxkblayout by-tech-gaming-keyboard next"
         "$mainMod, U, exec, hyprctl dispatch exec '[float; size 800 600; center] ghostty -e sh -c \"./~/.dotfiles/scripts/rebuild.sh; read -p \\\"Press Enter to close...\\\"\"'"
 
         # show keybinds list
