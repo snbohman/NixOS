86166e1aa205fd7ce908acd61afcb47da4cd6f73
snbohman <sixten.bohman.08@gmail.com>
2026-06-14 22:50:08 +0200

    2026-06-14 22:50:08

--- modules/home/scripts/scripts/wallpaper-picker.sh
+++ modules/home/scripts/scripts/wallpaper-picker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-wallpaper_path=$HOME/Pictures/wallpapers
-wallpapers_folder=$HOME/Pictures/wallpapers/others
+wallpaper_path=$HOME/.dotfiles/wallpapers
+wallpapers_folder=$HOME/.dotfiles/wallpapers/others
 wallpaper_name="$(ls $wallpapers_folder | rofi -dmenu || pkill rofi)"
 
 if [[ -f $wallpapers_folder/$wallpaper_name ]]; then
