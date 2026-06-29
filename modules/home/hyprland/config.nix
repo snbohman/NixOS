c652d14eb2b3ba0019837af3faacf672bbd42da4
snbohman <sixten.bohman.08@gmail.com>
2026-06-29 21:54:27 +0200

    2026-06-29 21:54:27

--- modules/home/hyprland/config.nix
+++ modules/home/hyprland/config.nix
@@ -177,7 +177,6 @@ in
         "$mainMod, N, exec, swaync-client -t -sw"
         "CTRL SHIFT, Escape, exec, hyprctl dispatch exec '[workspace 9] missioncenter'"
         "$mainMod, equal, exec, woomer"
-        # "$mainMod SHIFT, W, exec, vm-start"
 
         # screenshot
         ",Print, exec, screenshot --copy"
