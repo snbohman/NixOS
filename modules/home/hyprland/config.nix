4a15c18e5f4b4ecd03f0f245e3fe558e3b23df96
snbohman <sixten.bohman.08@gmail.com>
2026-06-29 17:59:59 +0200

    2026-06-29 17:59:59

--- modules/home/hyprland/config.nix
+++ modules/home/hyprland/config.nix
@@ -145,7 +145,7 @@ in
 
       bind = [
         "$mainMod SHIFT, K, exec, hyprctl switchxkblayout by-tech-gaming-keyboard next"
-        "$mainMod SHIFT, U, exec, ghostty --class=popup -- zsh ~/.dotfiles/scripts/rebuild_popup.sh"
+        "$mainMod SHIFT, U, exec, hyprctl dispatch exec '[float; size 800 900; center] ghostty --class=popup -- bash ~/.dotfiles/scripts/rebuild_popup.sh"
 
         # show keybinds list
         "$mainMod, F1, exec, show-keybinds"
