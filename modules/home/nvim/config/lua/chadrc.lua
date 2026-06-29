eaf00699100efc2164805e44d78cbaf84ed5e7dc
snbohman <sixten.bohman.08@gmail.com>
2026-06-29 21:58:45 +0200

    2026-06-29 21:58:45

--- modules/home/nvim/config/lua/chadrc.lua
+++ modules/home/nvim/config/lua/chadrc.lua
@@ -16,6 +16,6 @@ M.base46 = {
 
 M.nvdash.enabled = false
 M.ui.tabufline.enabled = false
-M.nvterm.terminals.type_opts.vertical.split_ratio=0.5
+-- M.nvterm.terminals.type_opts.vertical.split_ratio=0.5
 
 return M
