1d1b0b522d1ddd398083c55f49b4eb90a5e5cca5
snbohman <sixten.bohman.08@gmail.com>
2026-06-29 17:26:09 +0200

    2026-06-29 17:26:09

--- modules/home/fzf.nix
+++ modules/home/fzf.nix
@@ -2,28 +2,65 @@
 {
   programs.fzf = {
     enable = true;
-
     enableZshIntegration = true;
 
-    defaultCommand = "fd --hidden --strip-cwd-prefix --exclude .git";
-    fileWidgetOptions = [
-      "--preview 'if [ -d {} ]; then eza --tree --color=always {} | head -200; else bat -n --color=always --line-range :500 {}; fi'"
-    ];
-    changeDirWidgetCommand = "fd --type=d --hidden --strip-cwd-prefix --exclude .git";
-    changeDirWidgetOptions = [
-      "--preview 'eza --tree --color=always {} | head -200'"
-    ];
-
-    ## Theme
     defaultOptions = [
+      "--height=100%"
+      "--layout=reverse"
+      "--border=double"
+      "--preview-window=border-sharp"
+
       "--color=fg:-1,fg+:#FBF1C7,bg:-1,bg+:#282828"
       "--color=hl:#98971A,hl+:#B8BB26,info:#928374,marker:#D65D0E"
       "--color=prompt:#CC241D,spinner:#689D6A,pointer:#D65D0E,header:#458588"
       "--color=border:#665C54,label:#aeaeae,query:#FBF1C7"
-      "--border='double' --border-label='' --preview-window='border-sharp' --prompt='> '"
-      "--marker='>' --pointer='>' --separator='─' --scrollbar='│'"
-      "--info='right'"
-      "--bind change:top"
+
+      "--prompt='> '"
+      "--marker='>'"
+      "--pointer='>'"
+      "--separator='─'"
+      "--scrollbar='│'"
+      "--info=right"
     ];
+  };
+
+  programs.zsh = {
+    enable = true;
+
+    initExtra = ''
+      # Ctrl-G: recursive directory search + cd
+      fzf-dir-widget() {
+        local dir
+        dir=$(fd \
+          --type d \
+          --hidden \
+          --strip-cwd-prefix \
+          --exclude .git |
+          fzf) || return
+
+        cd "$dir"
+        zle reset-prompt
+      }
+
+      # Ctrl-H: recursive file search
+      fzf-file-widget() {
+        local file
+        file=$(fd \
+          --type f \
+          --hidden \
+          --strip-cwd-prefix \
+          --exclude .git |
+          fzf) || return
+
+        BUFFER="$file"
+        CURSOR=''${#BUFFER}
+      }
+
+      zle -N fzf-dir-widget
+      zle -N fzf-file-widget
+
+      bindkey '^G' fzf-dir-widget
+      bindkey '^H' fzf-file-widget
+    '';
   };
 }
