b142798fa482e56b98441c3fd86430a5c07e180b
snbohman <sixten.bohman.08@gmail.com>
2026-06-30 19:58:20 +0200

    2026-06-30 19:58:20

--- modules/home/zsh/zsh.nix
+++ modules/home/zsh/zsh.nix
@@ -2,24 +2,9 @@
 {
   programs.zsh = {
     enable = true;
-    autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
 
-    completionInit = ''
-      autoload -U compinit
-      compinit
-    '';
-
     initContent = ''
-      # Use fd (https://github.com/sharkdp/fd) for listing path candidates.
-      _fzf_compgen_path() {
-        fd --hidden --exclude .git . "$1"
-      }
-
-      _fzf_compgen_dir() {
-        fd --type=d --hidden --exclude .git . "$1"
-      }
-
       cdf_widget() {
         zle push-line
         BUFFER="cdf"
@@ -28,6 +13,9 @@
 
       zle -N cdf_widget
       bindkey '^G' cdf_widget
+
+      # Tab accepts the autosuggestion
+      bindkey '^I' autosuggest-accept
     '';
   };
 
