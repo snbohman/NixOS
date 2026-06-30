edb225003385ad39aa92f5a2fa8f807137425025
snbohman <sixten.bohman.08@gmail.com>
2026-06-30 16:10:39 +0200

    2026-06-30 16:10:39

--- modules/home/nvim/config/lua/mappings.lua
+++ modules/home/nvim/config/lua/mappings.lua
@@ -1,36 +1,27 @@
 require "nvchad.mappings"
 
--- add yours here
+vim.keymap.set('n', '<C-->', '<C-w>-')
+vim.keymap.set('n', '<C-+>', '<C-w>+')
+vim.keymap.set('n', '<C-<>', '<C-w><')
+vim.keymap.set('n', '<C->>', '<C-w>>')
 
-local map = vim.keymap.set
+vim.keymap.set('n', "<leader>o", ":wqa<CR>", {silent=true})
+vim.keymap.set('n', "<leader><leader>", ":so<CR>", {silent=true})
+vim.keymap.set('n', "<leader>dd", ":lua vim.diagnostic.disable()<CR>", { silent=true, desc="disable diagnostics"})
+vim.keymap.set('n', "<leader>de", ":lua vim.diagnostic.enable()<CR>", { silent=true, desc="enable diagnostics" })
+vim.keymap.set('n', "<leader>y", ":redo<CR>", {silent=true})
+vim.keymap.set('n', "leader>xd", ":vert term xxd<CR>", {silent=true})
+vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
 
-map("i", "jk", "<ESC>")
+vim.keymap.set("n", "<leader>B", ":hi Normal guibg=NONE ctermbg=NONE<CR>", { silent = true, desc = "remove bg color" })
+vim.keymap.set("n", "<leader>S", ":DBUIToggle<CR>", { silent = true, desc = "SQL" })
 
-map('n', '<C-->', '<C-w>-')
-map('n', '<C-+>', '<C-w>+')
-map('n', '<C-<>', '<C-w><')
-map('n', '<C->>', '<C-w>>')
+vim.keymap.set("n", "<leader>ce", ":vsplit | edit ~/.dotfiles/modules/home/nvim/config/lua/mappings.lua<CR>", { silent = true, desc = "edit config" })
 
-map('n', "<leader>o", ":wqa<CR>", {silent=true})
-map('n', "<leader><leader>", ":so<CR>", {silent=true})
-map('n', "<leader>pdd", ":lua vim.diagnostic.disable()<CR>", { silent=true, desc="disable diagnostics"})
-map('n', "<leader>pde", ":lua vim.diagnostic.enable()<CR>", { silent=true, desc="enable diagnostics" })
-map('n', "<leader>s", "$0v$h", {silent=true})
-map('n', "<leader>y", ":redo<CR>", {silent=true})
-map('n', "<leader>xd", ":%!xxd<CR>", {silent=true})
-map("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { noremap = true, silent = true })
-map("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
-map("n", "dd", "Vd", { silent = true })
-map("n", "<leader>B", ":hi Normal guibg=NONE ctermbg=NONE<CR>", { silent = true, desc = "remove bg color" })
-map("n", "<leader>e", ":lua vim.diagnostic.open_float()<CR>", { silent = true, desc = "popup diagnostic" })
-map("n", "<leader>E", ":lua vim.lsp.buf.hover() <CR>", { silent = true, desc = "popup description" })
-
-map("n", "<leader>S", ":DBUIToggle<CR>", { silent = true, desc = "SQL" })
-
-map("n", "<leader>ce", ":vsplit | edit ~/.dotfiles/modules/home/nvim/config/lua/mappings.lua<CR>", { silent = true, desc = "edit config" })
-
-map("n", "T", ":lua vim.diagnostic.goto_next()<CR>", { silent = true, desc = "goto next error" })
-map("n", "<C-f>", "V/\\%V", { silent = true, desc = "search line" })
+vim.keymap.set("n", "T", ":lua vim.diagnostic.goto_next()<CR>", { silent = true, desc = "goto next error" })
+vim.keymap.set("n", "<leader>e", ":lua vim.diagnostic.open_float()<CR>", { silent = true, desc = "popup diagnostic" })
+vim.keymap.set("n", "<leader>E", ":lua vim.lsp.buf.hover() <CR>", { silent = true, desc = "popup description" })
 
 vim.keymap.set("v", "<", "<gv")
 vim.keymap.set("v", ">", ">gv")
