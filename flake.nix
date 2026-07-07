7a913c7e2951a0120ff86b78c4e55e5b8b95d174
snbohman <sixten.bohman.08@gmail.com>
2026-07-07 16:38:50 +0200

    2026-07-07 16:38:50

--- flake.nix
+++ flake.nix
@@ -46,6 +46,11 @@
 
     nix-flatpak.url = "github:gmodena/nix-flatpak";
 
+    odin-nightly = {
+        url = "github:Yappaholic/odin-nightly";
+        inputs.nixpgks.follows = "nixpkgs";
+    };    
+
     zig-overlay = {
         url = "github:mitchellh/zig-overlay";
         inputs.nixpkgs.follows = "nixpkgs";
@@ -91,6 +96,7 @@
         secondary = nixpkgs.lib.nixosSystem {
           inherit system;
           modules = [ ./hosts/secondary ];
+          # modules = [ (nixpkgs.overlays = [inputs.odin-nightly.overlays.default]) ];
           specialArgs = {
             host = "secondary";
             inherit self inputs username;
