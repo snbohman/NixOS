cefd18bfc9d62985a2ef30bbed45a31b1899d76d
snbohman <sixten.bohman.08@gmail.com>
2026-07-07 15:57:41 +0200

    2026-07-07 15:57:41

--- modules/home/packages/dev.nix
+++ modules/home/packages/dev.nix
@@ -8,13 +8,14 @@
     premake5
     bear
     inputs.zig-overlay.packages.${pkgs.system}.master
+    odin
+    ols
 
     # language servers
     inputs.zls.packages.${pkgs.system}.zls
     nixd
     lua-language-server 
     llvmPackages_20.clang-tools
-
 
     ## Python
     python311
