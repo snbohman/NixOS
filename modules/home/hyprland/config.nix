a8e9ea431ed53935b3819a3c57f744adb7134ef5
snbohman <sixten.bohman.08@gmail.com>
2026-07-31 23:20:37 +0200

    2026-07-31 23:20:37

--- modules/home/hyprland/config.nix
+++ modules/home/hyprland/config.nix
@@ -35,7 +35,7 @@ in
         follow_mouse = 0;
         float_switch_override_focus = 0;
         mouse_refocus = 0;
-        sensitivity = -0.9;
+        sensitivity = -0.88;
         accel_profile = "flat";
         force_no_accel = false;
         touchpad = {
