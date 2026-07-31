0b89deee79978b7a3590c7db63027ff5658770d9
snbohman <sixten.bohman.08@gmail.com>
2026-07-31 11:28:53 +0200

    2026-07-31 11:28:53

--- modules/home/nvim/config/lua/plugins.lua
+++ modules/home/nvim/config/lua/plugins.lua
@@ -1,7 +1,5 @@
 vim.pack.add({
 	{ src = "https://github.com/neovim/nvim-lspconfig" },
-	{ src = "https://github.com/mason-org/mason.nvim" },
-	{ src = "https://github.com/mason-org/mason-lspconfig.nvim" },
 	{ src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
 	{ src = "https://github.com/ellisonleao/gruvbox.nvim" },
 	{ src = "https://github.com/stevearc/oil.nvim" },
@@ -17,8 +15,7 @@ vim.pack.add({
 require("multiple-cursors").setup()
 require("comment").setup()
 require("mini.pairs").setup()
-require("mason").setup()
-require("mason-lspconfig").setup()
+require("lspconfig").setup()
 require("oil").setup({
 	columns = {
 		"permissions",
