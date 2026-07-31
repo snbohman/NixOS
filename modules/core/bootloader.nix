2d6693b7c1d0d55e78f622a842967fdfb2b83fa6
snbohman <sixten.bohman.08@gmail.com>
2026-07-31 23:19:05 +0200

    2026-07-31 23:19:05

--- modules/core/bootloader.nix
+++ modules/core/bootloader.nix
@@ -2,7 +2,7 @@
 {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.systemd-boot.configurationLimit = 10;
+  boot.loader.systemd-boot.configurationLimit = 3;
   boot.kernelPackages = pkgs.linuxPackages_zen;
   boot.supportedFilesystems = [ "ext4" "btrfs" "vfat" "ntfs" ];
 }
--- modules/core/hardware.nix
+++ modules/core/hardware.nix
@@ -1,15 +1,24 @@
 { pkgs, config, ... }:
 {
   hardware.graphics.enable = true;
-  services.xserver.videoDrivers = ["nvidia"];
+  services.xserver.videoDrivers = [ "nvidia" ];
   hardware.uinput.enable = true;
 
   hardware.nvidia = {
     modesetting.enable = true;
     powerManagement.enable = false;
     powerManagement.finegrained = false;
-    open = true;
+
+    # Try changing this to false first
+    open = false;
+
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.latest;
+  };
+
+  environment.sessionVariables = {
+    LIBVA_DRIVER_NAME = "nvidia";
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    GBM_BACKEND = "nvidia-drm";
   };
 }
