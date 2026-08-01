25236f03f77af3037c3bebf4cb62248fcf016633
System administrator <root@nixos.localdomain>
2026-08-01 13:16:25 +0200

    xz compression

--- modules/core/bootloader.nix
+++ modules/core/bootloader.nix
@@ -2,7 +2,8 @@
 {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.systemd-boot.configurationLimit = 3;
+  boot.loader.systemd-boot.configurationLimit = 5;
   boot.kernelPackages = pkgs.linuxPackages_zen;
+  boot.initrd.compressor = "xz";
   boot.supportedFilesystems = [ "ext4" "btrfs" "vfat" "ntfs" ];
 }
--- modules/home/packages/dev.nix
+++ modules/home/packages/dev.nix
@@ -32,8 +32,5 @@ in {
     uv
 
     zsteg
-
-    evsieve
-    hello
   ];
 }
