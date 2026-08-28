a42c5649fff491c922b59a07133dffac846f7299
me <me>
2026-08-28 17:01:11 +0200

    2026-08-28 17:01:11

--- modules/core/bootloader.nix
+++ modules/core/bootloader.nix
@@ -1,9 +1,11 @@
 { pkgs, ... }:
 {
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.device = "nodev";
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.systemd-boot.configurationLimit = 1;
+  boot.loader.grub.configurationLimit = 5;
+  boot.loader.grub.useOSProber = true;
   boot.kernelPackages = pkgs.linuxPackages_zen;
-  boot.initrd.compressor = "xz";
   boot.supportedFilesystems = [ "ext4" "btrfs" "vfat" "ntfs" ];
 }
