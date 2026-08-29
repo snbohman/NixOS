12b645f19deb20e68aac61dec970722879422b03
me <me>
2026-08-29 12:36:58 +0200

    2026-08-29 12:36:58

--- modules/home/waybar/settings.nix
+++ modules/home/waybar/settings.nix
@@ -92,7 +92,7 @@ in
       on-click-right = "hyprctl dispatch exec '[float; center; size 950 650] kitty --override font_size=14 --title float_kitty btop'";
     } ;
     "hyprland/language" = {
-      format = "<span foreground='${red}'> </span>{} ";
+      format = "<span foreground='${red}'> </span>{}Ä";
       format-custom_se = "Custom";
       on-click = "hyprctl switchxkblayout by-tech-gaming-keyboard next";
     };
