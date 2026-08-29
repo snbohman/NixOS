e9245f5ff3b921ae8bd778fa41bdb05b8455dc1e
me <me>
2026-08-29 12:31:06 +0200

    2026-08-29 12:31:06

--- modules/home/waybar/settings.nix
+++ modules/home/waybar/settings.nix
@@ -92,9 +92,9 @@ in
       on-click-right = "hyprctl dispatch exec '[float; center; size 950 650] kitty --override font_size=14 --title float_kitty btop'";
     } ;
     "hyprland/language" = {
-      format = "<span foreground='${red}'>  </span>{}";
-      format-se = "Swedish ";
-      format-custom_se = "Coding ";
+      format = "<span foreground='${red}'> </span>{}";
+      format-se = "Swedish  ";
+      format-custom_se = "Custom ";
       on-click = "hyprctl switchxkblayout by-tech-gaming-keyboard next";
     };
   };
