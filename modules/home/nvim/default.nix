145aee25167b6e2f51b01f0250be7d07c766caf3
me <me>
2026-08-30 16:19:23 +0200

    2026-08-30 16:19:23

--- modules/home/nvim/default.nix
+++ modules/home/nvim/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, inputs, ... }:
+{ config, pkgs, inputs, ... }:
 let
   pkgsLatest = import inputs.nixpkgs-latest {
     inherit (pkgs) system;
@@ -9,13 +9,14 @@ let
     withPython3 = true;
     extraPython3Packages = ps: [ ps.pynvim ];
   };
+
+  # autonomus symlink
+  nvimDir = "${config.home.homeDirectory}/.config/nixos/modules/home/nvim/config";
 in {
   home.packages = [
     neovimWithPython
     python3WithPynvim
   ];
-home.file.".config/nvim" = {
-    source = ./config;
-    recursive = true;
-  };
+
+  home.file.".config/nvim".source = config.lib.file.mkOutOfStoreSymlink nvimDir;
 }
