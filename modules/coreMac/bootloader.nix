cf7d639845e618abdd487220380eea62c0125627
me <me>
2026-09-13 14:26:52 +0000

    2026-09-13 14:26:52

--- modules/coreMac/bootloader.nix
+++ modules/coreMac/bootloader.nix
@@ -1,24 +1,4 @@
 { pkgs, ... }:
-let
-  outer-wilds-grub = pkgs.stdenv.mkDerivation {
-    pname = "outer-wilds-grub-theme";
-    version = "unstable";
-
-    src = pkgs.fetchFromGitHub {
-      owner = "Terofale";
-      repo = "outer-wilds-grub-theme";
-      rev = "main";
-
-      # Fill this in with the hash Nix gives you.
-      hash = "sha256-XPVUbNzR8DIIJLdw7yQ9jNekJwerXVm55fvNAxfW1jQ=";
-    };
-
-    installPhase = ''
-      mkdir -p $out
-      cp -r theme-files-1080p/* $out/
-    '';
-  };
-in
 {
   boot.loader.grub = {
       enable = true;
@@ -26,9 +6,6 @@ in
       device = "nodev";
       configurationLimit = 5;
       useOSProber = true;
-      theme = outer-wilds-grub;
-      gfxmodeEfi = "1920x1080,auto";
-      gfxmodeBios = "1920x1080,auto";
   };
 
   boot.loader.efi.canTouchEfiVariables = true;
